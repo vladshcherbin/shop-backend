62f05890a4800cf8da191df98ee10c119feb72b9
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 10:35:08 +0300

    Extract products list, add more items

---
+++ services/products/src/data/products.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: 'High waist super skinny jeans'
+  },
+  {
+    id: 2,
+    title: 'Basic top'
+  },
+  {
+    id: 3,
+    title: 'Ripped mom jeans'
+  },
+  {
+    id: 4,
+    title: 'Skinny jeans'
+  },
+  {
+    id: 5,
+    title: 'ECO AWARE denim jacket'
+  },
+  {
+    id: 6,
+    title: 'ECO AWARE quilted jacket with hood'
+  }
+]
--- services/products/src/handlers/products-list.js
+++ services/products/src/handlers/products-list.js
@@ -1,15 +1,8 @@
+import products from '../data/products'
+
 export default async function productsList() {
   return {
     statusCode: 200,
-    body: JSON.stringify([
-      {
-        id: 1,
-        title: 'product 1'
-      },
-      {
-        id: 2,
-        title: 'product 2'
-      }
-    ])
+    body: JSON.stringify(products)
   }
 }
