7c09512ee22c46e85cca447cf5637387b395ac1b
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 12:35:08 +0300

    Update products list response object

--- services/products/src/handlers/products-list.js
+++ services/products/src/handlers/products-list.js
@@ -3,6 +3,6 @@ import products from '../data/products'
 export async function handler() {
   return {
     statusCode: 200,
-    body: JSON.stringify(products)
+    body: JSON.stringify({ products })
   }
 }
