186a21841a043082153bbd8c6f4341e6f9dea54f
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 12:33:43 +0300

    Add slug to products list

--- services/products/src/data/products.js
+++ services/products/src/data/products.js
@@ -1,26 +1,32 @@
 export default [
   {
     id: 1,
-    title: 'High waist super skinny jeans'
+    title: 'High waist super skinny jeans',
+    slug: 'high-waist-super-skinny-jeans'
   },
   {
     id: 2,
-    title: 'Basic top'
+    title: 'Basic top',
+    slug: 'basic-top'
   },
   {
     id: 3,
-    title: 'Ripped mom jeans'
+    title: 'Ripped mom jeans',
+    slug: 'ripped-mom-jeans'
   },
   {
     id: 4,
-    title: 'Skinny jeans'
+    title: 'Skinny jeans',
+    slug: 'skinny-jeans'
   },
   {
     id: 5,
-    title: 'ECO AWARE denim jacket'
+    title: 'ECO AWARE denim jacket',
+    slug: 'eco-aware-denim-jacket'
   },
   {
     id: 6,
-    title: 'ECO AWARE quilted jacket with hood'
+    title: 'ECO AWARE quilted jacket with hood',
+    slug: 'eco-aware-quilted-jacket-with-hood'
   }
 ]
