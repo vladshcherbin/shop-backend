50fdcbe39e5f192eb2510b9c9d957071aaac21d8
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 12:20:34 +0300

    Update eslint config default export rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: '@shcherbin/eslint-config/node'
+  extends: '@shcherbin/eslint-config/node',
+  overrides: [{
+    files: 'services/*/src/handlers/*.js',
+    rules: {
+      'import/prefer-default-export': 'off'
+    }
+  }]
 }
