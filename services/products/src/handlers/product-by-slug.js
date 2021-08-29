1329e45a8430b9474ba446e221ac4b5dd023d35c
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 12:34:52 +0300

    Replace product by id with product by slug

--- services/products/src/handlers/product-by-slug.js
+++ services/products/src/handlers/product-by-slug.js
@@ -1,12 +1,12 @@
 import products from '../data/products'
 
 export async function handler(event) {
-  const { id } = event.pathParameters
+  const { slug } = event.pathParameters
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      product: products.find((product) => product.id === Number(id))
+      product: products.find((product) => product.slug === slug)
     })
   }
 }
