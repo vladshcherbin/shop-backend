4305ede2f41a186cbe785a3fe2f55986ceb5a33d
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-08-29 12:44:14 +0300

    Add product not found error

--- services/products/src/handlers/product-by-slug.js
+++ services/products/src/handlers/product-by-slug.js
@@ -2,11 +2,17 @@ import products from '../data/products'
 
 export async function handler(event) {
   const { slug } = event.pathParameters
+  const foundProduct = products.find((product) => product.slug === slug)
+
+  if (!foundProduct) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'Product not found' })
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      product: products.find((product) => product.slug === slug)
-    })
+    body: JSON.stringify({ product: foundProduct })
   }
 }
