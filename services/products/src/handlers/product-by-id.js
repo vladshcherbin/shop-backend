4f1b73c1be4890f36da0b1509e53522a17b81f4a
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-09-22 19:43:12 +0300

    Add database to product by id handler

--- services/products/src/handlers/product-by-id.js
+++ services/products/src/handlers/product-by-id.js
@@ -1,24 +1,40 @@
-import { findById } from '../data/products'
+import knexConnection from 'knex'
+import logger from '../modules/logger'
+import errorResponse from '../modules/response/error-response'
+import notFoundResponse from '../modules/response/not-found-response'
+import successfulResponse from '../modules/response/successful-response'
 
-// TODO add db
-export async function handler(event) {
-  const product = await findById(Number(event?.pathParameters?.id))
-
-  if (!product) {
-    return {
-      statusCode: 404,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({ message: 'Product not found' })
+const knex = knexConnection({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+      rejectUnauthorized: false
     }
   }
+})
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({ product })
+export async function handler(event) {
+  try {
+    logger.info({ event }, 'get product')
+
+    const product = await knex
+      .select('products.id', 'products.title', 'products.description', 'products.price', 'products.image_url', 'stocks.count')
+      .from('products')
+      .innerJoin('stocks', 'products.id', 'stocks.product_id')
+      .where('products.id', Number(event?.pathParameters?.id))
+      .first()
+
+    if (!product) {
+      return notFoundResponse('Product not found')
+    }
+
+    return successfulResponse({ product })
+  } catch (error) {
+    return errorResponse(error)
   }
 }
