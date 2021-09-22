80e31e60718f8a4bd08a7fade61f961ff1a07db5
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-09-22 19:32:36 +0300

    Add responses

---
+++ services/products/src/modules/response/error-response.js
@@ -0,0 +1,29 @@
+import logger from '../logger'
+import ValidationError from '../validation/ValidationError'
+
+const headers = {
+  'Access-Control-Allow-Origin': '*'
+}
+
+export default function errorResponse(error) {
+  logger.error('Internal error', error)
+
+  if (error instanceof ValidationError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        message: error.message,
+        validationErrors: error.extensions.validationErrors
+      })
+    }
+  }
+
+  return {
+    statusCode: 500,
+    headers,
+    body: JSON.stringify({
+      message: 'Internal server error'
+    })
+  }
+}
