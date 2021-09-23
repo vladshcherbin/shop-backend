19d7256f6d0e1d0aeb6c880d01ce4f90c3ef72ef
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-09-23 23:03:03 +0300

    Add parsed file move to parsed folder

--- services/import/src/handlers/import-file-parser.js
+++ services/import/src/handlers/import-file-parser.js
@@ -1,4 +1,4 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { CopyObjectCommand, DeleteObjectCommand, GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import logFile from '../modules/csv'
 import logger from '../modules/logger'
 import { errorResponse, successfulResponse } from '../modules/response'
@@ -15,6 +15,21 @@ export async function handler(event) {
     }))
 
     await logFile(Body)
+
+    logger.info('copying parsed file')
+
+    await client.send(new CopyObjectCommand({
+      CopySource: `${record.s3.bucket.name}/${record.s3.object.key}`,
+      Bucket: record.s3.bucket.name,
+      Key: record.s3.object.key.replace(/^uploaded\//, 'parsed/')
+    }))
+
+    logger.info('deleting parsed file')
+
+    await client.send(new DeleteObjectCommand({
+      Bucket: record.s3.bucket.name,
+      Key: record.s3.object.key
+    }))
 
     return successfulResponse({ status: 'success' })
   } catch (error) {
